e4c0b46730fd3c16e96570884c1605d93c781a94
EllenBeliniDOliveira <117590080+EllenBeliniDOliveira@users.noreply.github.com>
2023-04-19 15:37:00 -0900

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int vetor_1488 [7];
+    int i;
+    
+    for(i= 0; i<7; i ++)
+    printf("Endereço da posição %d: %d\n", &vetor_1488[i]);
+   
+    
+   
+}
